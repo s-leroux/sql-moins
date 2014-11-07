221e4821b9ff8d2c774a0b8dcb535dd55707957f
Sylvain Leroux <sylvain@chicoree.fr>
2014-11-08 00:29:05 +0100

    Hack to set NLS_LANG

--- sqlm/console.py
+++ sqlm/console.py
@@ -15,6 +15,33 @@ except FileNotFoundError:
 
 atexit.register(readline.write_history_file, histfile)
 
+import locale
+from collections import defaultdict
+#
+# Infer NLS_LANG 
+if 'NLS_LANG' not in os.environ:
+    # According to POSIX, a program which has not called 
+    # setlocale(LC_ALL, '') runs using the portable 'C' 
+    # locale. Calling setlocale(LC_ALL, '') lets it use 
+    # the default locale as defined by the LANG variable.
+    #
+    #See https://docs.python.org/3/library/locale.html#locale.getdefaultlocale
+    locale.setlocale(locale.LC_ALL, '')
+    country, encoding = locale.getdefaultlocale()
+
+    encodings = defaultdict(lambda:'UTF8', {
+        'UTF-8': 'UTF8'
+    })
+
+    countries = defaultdict(lambda:'FRENCH_FRANCE', {
+        'fr_FR':    'FRENCH_FRANCE'
+    })
+
+    os.environ['NLS_LANG'] = "{}.{}".format(
+                                        countries[country.upper()],
+                                        encodings[encoding.upper()])
+    print(os.environ['NLS_LANG'])
+
 class Console:
     def run(self, interpreter, env):
         quit = False
