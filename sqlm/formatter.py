f7ae26c6f8fe79f0e5a2e82cd57e303f5cd067b1
Sylvain Leroux <sylvain@chicoree.fr>
2015-01-13 22:23:37 +0100

    Fixed number alignment in tabular output

--- sqlm/formatter.py
+++ sqlm/formatter.py
@@ -1,3 +1,4 @@
+from types import SimpleNamespace
 import functools
 
 class TabularFormatter:
@@ -6,18 +7,37 @@ class TabularFormatter:
         
         # See http://legacy.python.org/dev/peps/pep-0249/#cursor-attributes
         # for cursor.description fields
-        #print([item for item in result.cursor.description])
+        # print([item for item in result.cursor.description])
+
+        colnum = len(result.cursor.description)
+
+        columns = [SimpleNamespace(name=name, 
+                                    type_code=col_type.__name__,
+                                    width=1,
+                                    align='>' if col_type.__name__ in ('NUMBER') else '<')
+                     for name, col_type, *tail in result.cursor.description]
+
         keys = result.keys()
-        w = [len(str(item)) for item in keys]
+        wl = [len(str(item)) for item in keys]
 
         data = []
         for row in result:
             row = [item if item is not None else "(null)" for item in row]
-            w = [max(i[0],len(str(i[1]))) for i in zip(w,row)]
+            for col, value in zip(columns, row):
+                w = len(str(value))
+                col.width = max(col.width, w)
             data.append(row)
 
-        fmt = " " + " | ".join(["{!s:"+str(item)+"}" for item in w]) + " "
-        sep = "-" + "-+-".join(["-"*item for item in w])           + "-"
+        fmt_list = []
+        for idx, column in enumerate(columns):
+            w = column.width
+
+            fmt_list.append("{!s:"+column.align + str(w)+"}")
+
+        print(columns)
+
+        fmt = " " + " | ".join(fmt_list) + " "
+        sep = "-" + "-+-".join(["-"*item for item in wl])           + "-"
 
         #print(data)
         #print(fmt)
