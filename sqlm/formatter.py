8d6d7533658ceb204c0a975c4d5cf92c99783870
Sylvain Leroux <sylvain@chicoree.fr>
2015-01-13 19:31:42 +0100

    Force convertion to string when displaying data

--- sqlm/formatter.py
+++ sqlm/formatter.py
@@ -6,7 +6,7 @@ class TabularFormatter:
         
         # See http://legacy.python.org/dev/peps/pep-0249/#cursor-attributes
         # for cursor.description fields
-        # print([item for item in result.cursor.description])
+        #print([item for item in result.cursor.description])
         keys = result.keys()
         w = [len(str(item)) for item in keys]
 
@@ -16,13 +16,14 @@ class TabularFormatter:
             w = [max(i[0],len(str(i[1]))) for i in zip(w,row)]
             data.append(row)
 
-        fmt = " " + " | ".join(["{:"+str(item)+"}" for item in w]) + " "
+        fmt = " " + " | ".join(["{!s:"+str(item)+"}" for item in w]) + " "
         sep = "-" + "-+-".join(["-"*item for item in w])           + "-"
 
         #print(data)
         #print(fmt)
         print(fmt.format(*keys))
-        print(sep)
+        #print(sep)
         for row in data:
+            #print(row)
             print(fmt.format(*row))
             
